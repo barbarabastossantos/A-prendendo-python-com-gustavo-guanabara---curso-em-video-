650469308a0248006795fe4b0b590e11b5508096
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-11-22 20:29:52 -0300

    primeirsa versao

---
+++ exoo2.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma messagem de boas vindas
+
+nome=input('Qual é o seu nome?')
+print(nome , ' , prazer em te conhecer?')
+
+#A solução do professor foi :
+# nome=input('digite seu nome:')
+# print('E um prazer te conhecer {} !' .format(nome))
