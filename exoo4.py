fbb2b5bd7691ec759b9c945fc0e51747ca577a61
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-11-23 07:01:16 -0300

    primeirsa versao

---
+++ exoo4.py
@@ -0,0 +1,3 @@
+n1=int(input('digite um numero)'))
+n2=int(input('digite outro numero'))
+print(f'o resultado e {n1+n2}')
---
+++ exoo5.py
@@ -0,0 +1,5 @@
+#DESAFIO
+# faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor
+
+n=int(input('Por gentileza, digite um numero inteiro : '))
+print(f' O número {n} tem como seu sucessor o numero {n+1} e como seu antecessor o numero {n-1}. Agradecemos sua colaboração no nosso programa!')
