21f56a411d253aad88715166e0a68f0c884e5aea
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-11-24 16:27:30 -0300

    avançando nos estudos de python com os operadores aritmeticos

---
+++ exoo7.py
@@ -0,0 +1,5 @@
+#DESAFIO 07 Desenvolver um programa que leia as duas notas de um aluno. Calcule a sua media
+
+n1=float(input('========================================== \n Preparado(a) para saber sua media ? Entao vamos lá ! \n Digite sua primeira nota : '))
+n2=float(input('Digite sua segunda nota : '))
+print(f'========================================= \n  CALCULANDO............ \n Sua media foi de {(n1+n2) / 2}  \n ==================================== \n AGRADECEMOS POR FAZER USO DO NOSSO APLICATIVO! \n ==============================================')
