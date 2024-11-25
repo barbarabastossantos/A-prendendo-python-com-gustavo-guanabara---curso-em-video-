b931ca9bf86871b5e67edb6c6334ea01bdfe255a
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-11-25 07:38:41 -0300

    Pintando parede com tinta Bastos

---
+++ exoo11.py
@@ -0,0 +1,9 @@
+#desafio 11  pintando parede
+
+al=float(input('=====================\n VAMOS PINTAR A PAREDE!!\n Para isso vamos ver quanto de tinta nescessaria por metros quadrados. \n Qual é a altura da sua parede  em metros ? '))
+la=float(input('Agora digite a largura da parede em metros : '))
+a= al*la
+print('======================\n V E R I F I C A N D O \n ======================')
+print(f' Sua parede tem de altura {al}m   x  {la}m  de largura , portanto ela tem  {a} de area em m². \n Com um exelente rendimento , a tinta  Bastos pinta 2 metros quadrados  por litro. ')
+print(f' Ao usar nossa tinta  verificando que sua area e de {a} m²  você precisará apenas de {a/2} litros de tinta.')
+print('====================================== \n MUITO OBRIGADA POR USAR NOSSO PLIOCATIVO E NÃO SE ESQUEÇA \n QUER ECONOMIZAR USE AS TINTAS BASTOS!!')
