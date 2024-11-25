f672e7c46282d49d7b7c6e5cfbf203d3c957b336
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-11-24 22:44:21 -0300

    convertendo real em dolar

--- exoo10.py
+++ exoo10.py
@@ -1,4 +1,4 @@
 #DESAFIO 10 conversor de moeda
 
-d=float(input('====================== \n Bem vindo(a) a nossa Casa de Câmbio App! \n Uma solução virtual para quem precisa de praticidade. \n Vamos lá ! \n Quanto de dinheiro em reais você quer fazer  de cambio em dolar ? '))
+d=float(input('====================== \n Bem vindo(a) a nossa Casa de Câmbio App! \n Uma solução virtual para quem precisa de praticidade. \n Vamos lá ! \n Quanto de dinheiro em reais você quer fazer  de cambio em dolar ? R$'))
 print(f' ============================ \n O dolar atualmente esta no valor de R$ 5.81  , você tem {d} vamos fazer a conversão \n ========== C O N V E R T E N D O ========== \n  Com R$ {d}  você  consegue US$ { d / 5.81 :.2f}')
