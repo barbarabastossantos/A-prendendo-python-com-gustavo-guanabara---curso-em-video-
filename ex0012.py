0544d3d4f4522bedfb8cb9a9c8a30040116e36c2
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-11-27 07:22:46 -0300

    calculando desconto

---
+++ ex0012.py
@@ -0,0 +1,4 @@
+p=float(input('============================== \n Informe o preço do produto : R$  '))
+r= p * 5 /100
+v= p - r
+print(f' O preço do produto que custava {p} R$ , \n na promoçâo com desconto de 5% vai R$ {v:.2f} e \n você teve R$ {r:.2f} de desconto . ')
