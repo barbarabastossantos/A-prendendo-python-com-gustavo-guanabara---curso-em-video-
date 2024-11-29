966f1e9657026ddf658cedc3ea1bdcbf0d76015e
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-11-29 07:26:39 -0300

    USANDO EMOJIS _ MODULOS

---
+++ exoo15.py
@@ -0,0 +1,7 @@
+import emoji
+ #print(emoji.emojize("ola,  milena  menina veia feia ! : smile: "))
+#print(emoji.emojize("Barbara esta na parada jogando duro :books: : computer: "))
+
+print("\U0001F92B	")
+print("Finalmente consegui usar emojis \U0001F937   \U0001F92D	\U0001F440	")
+print("\U0001F911	")
