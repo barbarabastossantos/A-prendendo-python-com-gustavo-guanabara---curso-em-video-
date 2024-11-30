562d9fde07c291d208586981306ade32b67e71c9
Barbara Bastos <barbara.bastos1995@gmail.com>
2024-11-30 19:09:01 -0300

    usando a função TRUNC

---
+++ exoo16.py
@@ -0,0 +1,7 @@
+#desafio de usar a função trunc
+#curso em viodeo
+#barbara BASTOS
+from math import trunc
+import math
+n=float(input('digite um numero real :'))
+print(f' o numero {n} tem parte inteira {trunc(n)}')
